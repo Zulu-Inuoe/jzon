4f8ee981ad1605382eb862404565a9d5daf78d10
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-12-27 13:04:53 -0500

    Have the JSONTestSuite test return 1 if any test fails

--- JSONTestSuite/test.bat
+++ JSONTestSuite/test.bat
@@ -1,20 +1,22 @@
 @setlocal EnableDelayedExpansion
 
+@set failed=0
+
 @for /r %%i in (test_parsing\*) do @(
   set "name=%%~ni"
   echo|set /p="%%~nxi ... "
   "%~dp0bin\jzon-parsing" "%%i"
 
-  set errlvl=!ERRORLEVEL!
-
-  if !errlvl! EQU 0 (
+  if !ERRORLEVEL! EQU 0 (
     if "!name:~0,1!" == "n" (
+      set failed=1
       echo ERROR PASSED
     ) else (
       echo OK
     )
-  ) else if !errlvl! EQU 1 (
+  ) else if !ERRORLEVEL! EQU 1 (
     if "!name:~0,1!" == "y" (
+      set failed=1
       echo ERROR FAILED
     ) else (
       echo OK
@@ -23,5 +25,7 @@
     echo ERROR !errlvl!
   )
 )
+
+@if %failed% neq 0 exit /b %failed%
 
 @endlocal
